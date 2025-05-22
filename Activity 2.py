adf9bc7939b7d4f277a5dcfc1ca580ef1aa7e583
Hamza7655code <mughis.rehman@yahoo.com>
2025-05-22 18:11:30 +0100

    Update Activity 2.py

--- Activity 2.py
+++ Activity 2.py
@@ -1,9 +1,9 @@
 for i in range(20):
-    if i % 20:
+    if i % 20==0:
         print("twist")
-    elif i % 15:
+    elif i % 15==0:
         pass
-    elif i % 5:
+    elif i % 5==0:
         print("buzz")
     else:
         print(i)
